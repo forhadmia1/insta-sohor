d902406c23487174cda329d4a5cf92b5dc700bc3
forhadmia1 <forhad8613@gmail.com>
2022-03-10 21:34:49 +0600

    fix the issue and show only the reported posts

--- js/app.js
+++ js/app.js
@@ -152,7 +152,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    document.getElementById( "reported" ).textContent= '';
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
